92b086ddf7d7669a38a22e018d4135eaa973f6ad
NanaEsiE <nesi.eyiah@gmail.com>
2023-08-24 18:49:04 -0700

    singly linked

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,16 @@
+SECTION .data
+msg:	db "Hello, Holberton", 0
+fmt:	db "%s", 10, 0
+
+	SECTION .text
+	extern printf
+	global main
+
+main:
+	mov esi, msg
+	mov rdi, fmt
+	mov rax, 0
+	call printf
+
+	mov rax, 0
+	ret

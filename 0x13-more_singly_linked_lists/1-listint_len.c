3bb3ca79470c3291046d8c68bd26300652099a48
NanaEsiE <nesi.eyiah@gmail.com>
2023-08-29 07:11:30 -0700

    num 1

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,38 +1,17 @@
-#ifndef _LISTS_
-#define _LISTS_
-
-#include <stdio.h>
-#include <stdlib.h>
+#include "lists.h"
 
 /**
- * struct listint_s - singly linked list used.
- * @s: integer.
- * @next: next node
- *
- * Description: singly linked list node structure used.
- * alx se project for Nana Esi
+ * listint_len - returns num of elements in a listint_t list.
+ * @p: list.
  * 
+ * Return: nodes
  */
-typedef struct listint_s
+
+size_t listint_len(const listint_t *p)
 {
-    int s;
-    struct listint_s *next;
-} listint_t;
+	size_t i;
 
-size_t print_listint(const listint_t *h);
-size_t listint_len(const listint_t *h);
-listint_t *add_nodeint(listint_t **head, const int s);
-listint_t *add_nodeint_end(listint_t **head, const int s);
-void free_listint(listint_t *head);
-void free_listint2(listint_t **head);
-int pop_listint(listint_t **head);
-listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
-int sum_listint(listint_t *head);
-listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int s);
-int delete_nodeint_at_index(listint_t **head, unsigned int index);
-listint_t *reverse_listint(listint_t **head);
-size_t print_listint_safe(const listint_t *head);
-size_t free_listint_safe(listint_t **h);
-listint_t *find_listint_loop(listint_t *head);
-
-#endif
+	for (i = 0; p; i++)
+		p = p->next;
+	return (i);
+}

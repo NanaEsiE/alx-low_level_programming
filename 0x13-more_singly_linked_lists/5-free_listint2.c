6bc2eac0efeccb82117473a6b596bb64d85f0730
NanaEsiE <nesi.eyiah@gmail.com>
2023-08-30 16:05:15 -0700

    5-free_listint2.c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -12,13 +12,12 @@ void free_listint2(listint_t **head)
 {
 	listint_t *temp;
 
-	if (head != NULL)
+	if (head == NULL)
 		return;
 
-	else
-	*head
+	while (*head)
 	{
-		temp = *head->next;
+		temp = (*head)->next;
 		free(*head);
 		*head = temp;
 	}
